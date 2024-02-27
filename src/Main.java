2921e68429e4d20f1f1a3d7bc74f314ac6e004c5
Aria Lesmana <arixlsmn@gmail.com>
2024-02-27 16:34:42 +0700

    test 1

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,14 @@
 
 public class Main { //deklarasi class java, biasanya nama Main digunakan untuk class yang menjalankan fungsi utama program
-    public static void main(String[] args) { //deklarasi fungsi java, khusus untuk fungsi "public static void main"
-                                            // digunakan sebagai fungsi utama untuk me-run sebuah project java
-                                            //dalam kurung terdapat parameter berupa "String[] args","String[]" berupa tipe data variabel
-                                            //args berupa nama dari variabel argumen dalam parameter
+    public static void main(String[] args) { /* deklarasi fungsi java yang disebut sebagai method, khusus untuk fungsi "public static void main"
+                                            digunakan sebagai fungsi utama untuk me-run sebuah project java
+                                            dalam kurung terdapat parameter berupa "String[] args","String[]" berupa tipe data variabel
+                                            args berupa nama dari variabel argumen dalam parameter */
 
-        System.out.println("Hello world!");//fungsi System.out.println digunakan untuk melakukan print suatu teks ke
-                                            // console disertai newline, jika teks dalam argumen berisi "Hello world!",
-                                            // maka "Hello world!" akan ditampilkan di console
-                                            //setiap fungsi java yang dipanggil diakhiri dengan titik koma
-    }                                       //sedangkan deklarasi kelas, fungsi atau loop menggunakan kurung kurawal
+        System.out.println("Hello world!");/* fungsi System.out.println digunakan untuk melakukan print suatu teks ke
+                                            console disertai newline, jika teks dalam argumen berisi "Hello world!",
+                                            maka "Hello world!" akan ditampilkan di console
+                                            setiap fungsi java yang dipanggil diakhiri dengan titik koma
+                                           sedangkan deklarasi kelas, fungsi atau loop menggunakan kurung kurawal */
+    }
 }
