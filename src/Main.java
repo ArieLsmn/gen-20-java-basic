8c7ef0e546eee042cb8d2437a3b226b88bf648d8
Aria Lesmana <arixlsmn@gmail.com>
2024-02-27 16:55:42 +0700

    test 1

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,13 @@
 
 public class Main { //deklarasi class java, biasanya nama Main digunakan untuk class yang menjalankan fungsi utama program
-    public static void main(String[] args) { /* deklarasi fungsi java yang disebut sebagai method, khusus untuk fungsi "public static void main"
-                                            digunakan sebagai fungsi utama untuk me-run sebuah project java
-                                            dalam kurung terdapat parameter berupa "String[] args","String[]" berupa tipe data variabel
+
+    public static void main(String[] args) { /* deklarasi fungsi buatan, yang disebut sebagai method,
+                                            khusus untuk fungsi "public static void main" digunakan sebagai fungsi utama
+                                            untuk me-run sebuah project java, dalam kurung terdapat parameter berupa
+                                            "String[] args","String[]" berupa tipe data variabel
                                             args berupa nama dari variabel argumen dalam parameter */
 
+        System.out.print("Hello!");     /* fungsi System.out.println digunakan untuk melakukan print suatu teks ke console */
         System.out.println("Hello world!");/* fungsi System.out.println digunakan untuk melakukan print suatu teks ke
                                             console disertai newline, jika teks dalam argumen berisi "Hello world!",
                                             maka "Hello world!" akan ditampilkan di console
