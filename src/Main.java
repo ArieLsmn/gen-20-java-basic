11600bcf48da03b9aaae97192215520a345444ca
Aria Lesmana <32252675+ArieLsmn@users.noreply.github.com>
2024-02-28 10:02:16 +0700

    Update dokum

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,12 @@
 
-public class Main { //deklarasi class java, biasanya nama Main digunakan untuk class yang menjalankan fungsi utama program
+public class Main { // deklarasi class java, biasanya nama Main digunakan untuk class yang menjalankan fungsi utama program
 
-    public static void main(String[] args) { /* deklarasi fungsi buatan, yang disebut sebagai method,
-                                            khusus untuk fungsi "public static void main" digunakan sebagai fungsi utama
-                                            untuk me-run sebuah project java, dalam kurung terdapat parameter berupa
-                                            "String[] args","String[]" berupa tipe data variabel
-                                            args berupa nama dari variabel argumen dalam parameter */
+    
+    
+    public static void main(String[] args) { // deklarasi fungsi custom, yang disebut sebagai method, khusus untuk fungsi "public static void main" digunakan sebagai fungsi utama untuk me-run sebuah project java
 
-        System.out.print("Hello!");     /* fungsi System.out.println digunakan untuk melakukan print suatu teks ke console */
-        System.out.println("Hello world!");/* fungsi System.out.println digunakan untuk melakukan print suatu teks ke
-                                            console disertai newline, jika teks dalam argumen berisi "Hello world!",
-                                            maka "Hello world!" akan ditampilkan di console
-                                            setiap fungsi java yang dipanggil diakhiri dengan titik koma
-                                           sedangkan deklarasi kelas, fungsi atau loop menggunakan kurung kurawal */
+        System.out.print("Hello!");     // fungsi System.out.print digunakan untuk melakukan print suatu teks ke console
+        
+        System.out.println("Hello world!"); // fungsi System.out.println digunakan untuk melakukan print suatu teks ke console disertai newline
     }
 }
